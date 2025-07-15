c6350d4fac5d9f01d1286079c8cc85d858193b56
amanxsays <amanjhasahab07@gmail.com>
2025-07-15 14:54:35 +0530

    initializing the front end

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Body from "./Body"
+import Login from "./Login"
+import { BrowserRouter, Routes , Route } from "react-router-dom"
+import Profile from "./Profile"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
