452200886a49cc92d9c2aedbc19d290d894cb423
amanxsays <amanjhasahab07@gmail.com>
2025-07-30 00:17:22 +0530

    changed while creating connnectiion with socket when on deploy

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,6 @@ import {io} from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection=()=>{
-    return io(BASE_URL);// THIS WILL ONLY WORK FOR developemnt
+    if(location.hostname==="localhost") return io(BASE_URL);// THIS WILL ONLY WORK FOR developemnt
+    else return io("/",{ path:"/api/socket.io"})
 }
