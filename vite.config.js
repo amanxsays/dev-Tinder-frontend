2b0c2e282908f3f55689684b64eab1ecc90195c1
amanxsays <amanjhasahab07@gmail.com>
2026-01-21 12:51:09 +0530

    allowed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss() ]
+  plugins: [react() , tailwindcss() ],
+  preview: {
+    allowedHosts: [
+      "dev-tinder-frontend-production.up.railway.app",
+    ],
+  },
 })
