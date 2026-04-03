246195c6b324821ca6560c40d305055c662df586
amanxsays <amanjhasahab07@gmail.com>
2026-04-03 20:46:54 +0530

    hardcoded the backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL= location.hostname==="localhost" ? "http://localhost:7777" : "/api";
-export const BASE_URL=import.meta.env.VITE_API_URL;
+export const BASE_URL="https://dev-tinder-backend-lmw0.onrender.com";
