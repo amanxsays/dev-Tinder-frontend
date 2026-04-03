e4f0c8a5a5e882729a46fa491c758648c16d1854
amanxsays <amanjhasahab07@gmail.com>
2026-04-03 20:32:20 +0530

    added env again

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react() , tailwindcss() ],
-  preview: {
-    allowedHosts: [
-      "dev-tinder-frontend-production.up.railway.app",
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    preview: {
+      allowedHosts: [
+        env.VITE_API_URL
+      ]
+    }
+  }
 })
